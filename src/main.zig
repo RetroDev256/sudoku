63467896dffd6fcc45b6e28e8ed157da560c8401
Eric Petersen <ericpetersen3.14@gmail.com>
2025-04-23 23:10:46 -0600

    we do a bit of tomfoolery

--- src/main.zig
+++ src/main.zig
@@ -22,8 +22,8 @@ fn posixMainAndExit(argc_argv_ptr: [*]usize) callconv(.c) noreturn {
         cell += 1;
     }
 
-    if (solve(argv[1][0..81].*)) |solved| {
-        try render(solved);
+    if (solve(argv[1][0..81])) {
+        try render(argv[1][0..81].*);
     }
 
     std.process.exit(0);
@@ -33,8 +33,8 @@ const std = @import("std");
 const assert = std.debug.assert;
 
 // Backtracking sudoku solver
-fn solve(grid: [81]u8) ?[81]u8 {
-    var state: [81]u8 = grid;
+fn solve(state: *[81]u8) bool {
+    const grid: [81]u8 = state.*;
     var current: u32 = 0;
 
     while (true) {
@@ -42,13 +42,13 @@ fn solve(grid: [81]u8) ?[81]u8 {
             // move forward to the next non-zero cell
             if (state[current] != 0) {
                 if (current == 80) {
-                    return state; // found solution
+                    return true; // found solution
                 } else {
                     current += 1;
                     // skip cells we can't change
                     while (grid[current] != 0) {
                         if (current == 80) {
-                            return state; // found solution
+                            return true; // found solution
                         } else {
                             current += 1;
                         }
@@ -64,7 +64,7 @@ fn solve(grid: [81]u8) ?[81]u8 {
                 // search for the last cell which we can change
                 while (grid[current] != 0) {
                     if (current == 0) {
-                        return null; // no solution
+                        return false; // no solution
                     } else {
                         current -= 1;
                     }
@@ -75,11 +75,11 @@ fn solve(grid: [81]u8) ?[81]u8 {
                 state[current] -= 1;
 
                 // stop backtracking when we are in a stable state
-                if (state[current] > 0) {
+                if (state[current] != 0) {
                     break :backtrack;
                 } else {
                     if (current == 0) {
-                        return null; // no solution
+                        return false; // no solution
                     } else {
                         current -= 1;
                     }
@@ -106,40 +106,38 @@ fn putstr(str: []const u8) void {
 }
 
 // return true on success - ignore cells == 0
-fn check(grid: [81]u8) bool {
-    return rows(grid) and cols(grid) and blocks(grid);
+fn check(grid: *const [81]u8) bool {
+    return lines(grid) and blocks(grid);
 }
 
 // return true on success - ignore cells == 0
-fn rows(grid: [81]u8) bool {
-    for (0..9) |row| {
-        var mask: u32 = 0;
-        for (0..9) |col| {
-            const idx = col + row * 9;
-            if (testMask(grid[idx], &mask)) {
-                return false;
+fn lines(grid: *const [81]u8) bool {
+    for (0..9) |col_a| {
+        for (0..col_a) |col_b| {
+            for (0..9) |row_a| {
+                for (0..row_a) |row_b| {
+                    const idx = col_a + row_a * 9;
+                    if (grid[idx] == 0) continue;
+
+                    const row_idx = col_b + row_a * 9;
+                    if (grid[idx] == grid[row_idx]) {
+                        return false;
+                    }
+
+                    const col_idx = col_a + row_b * 9;
+                    if (grid[idx] == grid[col_idx]) {
+                        return false;
+                    }
+                }
             }
         }
     }
+
     return true;
 }
 
 // return true on success - ignore cells == 0
-fn cols(grid: [81]u8) bool {
-    for (0..9) |col| {
-        var mask: u32 = 0;
-        for (0..9) |row| {
-            const idx = col + row * 9;
-            if (testMask(grid[idx], &mask)) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-// return true on success - ignore cells == 0
-fn blocks(grid: [81]u8) bool {
+fn blocks(grid: *const [81]u8) bool {
     for (0..3) |b_row| {
         for (0..3) |b_col| {
             var mask: u32 = 0;
@@ -148,27 +146,19 @@ fn blocks(grid: [81]u8) bool {
                     const row = sub_row + b_row * 3;
                     const col = sub_col + b_col * 3;
                     const idx = col + row * 9;
+                    if (grid[idx] == 0) continue;
 
-                    if (testMask(grid[idx], &mask)) {
+                    const shift: u5 = @intCast(grid[idx]);
+
+                    if ((mask >> shift) & 1 == 1) {
                         return false;
+                    } else {
+                        mask |= @as(u32, 1) << shift;
                     }
                 }
             }
         }
     }
-    return true;
-}
 
-// return true if the mask already contains that value
-fn testMask(val: u8, mask: *u32) bool {
-    if (val != 0) {
-        const shift: u5 = @intCast(val - 1);
-        const bit = @as(u32, 1) << shift;
-        if (mask.* & bit != 0) {
-            return true;
-        } else {
-            mask.* |= bit;
-        }
-    }
-    return false;
+    return true;
 }
